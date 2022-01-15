59fe70ba0043e7b1b857ac666cd0f7318eeb9154
japarici <aparici@gsol.es>
2022-01-15 08:05:01 +0100

    Añadido modulo document_crm_ext

---
+++ document_crm_ext/__manifest__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#????????????????????????????????????????????????????????????????????????????
+#?                                                                          ?
+#?           ,o888888o.      d888888o.       ,o888888o.     8 8888          ?
+#?          8888     `88.  .`8888:' `88.  . 8888     `88.   8 8888          ?
+#?       ,8 8888       `8. 8.`8888.   Y8 ,8 8888       `8b  8 8888          ?
+#?       88 8888           `8.`8888.     88 8888        `8b 8 8888          ?
+#?       88 8888            `8.`8888.    88 8888         88 8 8888          ?
+#?       88 8888             `8.`8888.   88 8888         88 8 8888          ?
+#?       88 8888   8888888    `8.`8888.  88 8888        ,8P 8 8888          ?
+#?       `8 8888       .8'8b   `8.`8888. `8 8888       ,8P  8 8888          ?
+#?          8888     ,88' `8b.  ;8.`8888  ` 8888     ,88'   8 8888          ?
+#?           `8888888P'    `Y8888P ,88P'     `8888888P'     8 888888888888  ? 
+#?                                                                          ?
+#?     SOFTWARE DEVELOPED AND SUPPORTED BY GSol Soluciones Informácas       ?
+#?                       COPYRIGHT (C) 2020 - TODAY                         ?
+#?                           http://www.gsol.es                             ?
+#?                                                                          ?
+#????????????????????????????????????????????????????????????????????????????
+{
+    'name': "Documents CRM",
+
+    'summary': """
+	Add Crm to the Documents module
+    """,
+
+    'description': """
+        Add Crm to the Documents module:
+        • User can add documents by opportunity
+        • Added smart button in Opportunity View to show and add Documents
+        • Added textbox at Documents module to assign Opportunity to documents
+        • Search by Opportunity at Documents module
+    """,
+
+    'license': 'LGPL-3',
+    'author': "GSol Soluciones Informaticas",
+    'website': "https://www.gsol.es",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Documents',
+    'version': '15.1.1',
+
+    'depends': ['base', 'documents', 'crm'],
+    'assets': {
+        'web.assets_backend': [
+            'document_crm_ext/static/src/js/document_crm.js',
+        ],
+    },
+    'data': [
+        #'data/cron.xml',
+        'views/templates.xml',
+        'views/crm_lead.xml'
+    ],
+}
---
+++ document_crm_ext/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+import base64
+
+
+class Document(models.Model):
+    _inherit = "documents.document"
+
+    crm_id = fields.Many2one("crm.lead", string="Opportunity")
+
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+
+    document_count = fields.Integer(string='Documents', compute="_compute_document_count")
+
+    def auto_check_document_exist(self):
+        lead_obj = self.env['crm.lead'].search([('document_count','>',0),('exist_docs','!=',True)])
+        folder_id = self.env['documents.folder'].search([('name', '=', 'Marketing')])
+        for lead in lead_obj:
+            document_ids = self.env['documents.document'].search_count([('crm_id', '=', lead.id),('folder_id','=',folder_id.id)])
+            if document_ids:
+                lead.exist_docs = True
+
+    def _compute_document_count(self):
+        for rec in self:
+            rec.document_count = self.env['documents.document'].search_count([('crm_id', '=', rec.id)])
+
+    def action_open_crm_documents(self):
+        self.ensure_one()
+        return {
+            'name': _('Documents'),
+            'res_model': 'documents.document',
+            'type': 'ir.actions.act_window',
+            'views': [(False, 'kanban')],
+            'view_mode': 'kanban',
+            'context': {
+                "search_default_crm_id": self.id,
+                "default_crm_id": self.id,
+                "searchpanel_default_folder_id": False
+            },
+        }
