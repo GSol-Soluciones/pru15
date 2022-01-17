cb4e941af102696139a1ea67788616c7077b11a7
japarici <aparici@gsol.es>
2022-01-17 14:38:11 +0100

    Cambios Doc CRM indio

--- document_crm_ext/__manifest__.py
+++ document_crm_ext/__manifest__.py
@@ -32,7 +32,7 @@
         • Search by Opportunity at Documents module
     """,
 
-    'license': 'LGPL-3',
+    'license': "OPL-1",
     'author': "GSol Soluciones Informaticas",
     'website': "https://www.gsol.es",
 
@@ -40,7 +40,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Documents',
-    'version': '15.1.1',
+    'version': '15.2.1',
 
     'depends': ['base', 'documents', 'crm'],
     'assets': {
--- document_crm_ext/controllers/main.py
+++ document_crm_ext/controllers/main.py
@@ -8,6 +8,7 @@ class ShareRouteExt(ShareRoute):
 
     @http.route()
     def upload_document(self, **kwargs):
+        kwargs.update({'tag_ids':''})
         documents_res = super(ShareRouteExt, self).upload_document(**kwargs)
         documents = json.loads(documents_res.data).get('ids', False)
         documents = documents and request.env['documents.document'].sudo().browse(documents)
